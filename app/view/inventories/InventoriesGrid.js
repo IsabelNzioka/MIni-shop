cce51f16807207d75ed2882f8506c6a8c6ed7fea
IsabelNzioka <isabelnzioka@gmail.com>
2024-04-03 12:30:31 +0300

    Enable Inventory cell editing

--- app/view/inventories/InventoriesGrid.js
+++ app/view/inventories/InventoriesGrid.js
@@ -2,33 +2,45 @@ Ext.define("Mini-shop.view.inventories.InventoriesGrid", {
     extend: "Ext.grid.Panel",
     xtype: "inventoriesgrid",
     reference: "inventoriesgrid",
-
     controller: "inventorycontroller",
-
     store: {
         type: 'inventories'
     },
 
-    tbar:[{
+    tbar: [{
         text: 'Add Inventories',
         listeners: {
             click: 'onAddInventoriesClicked'
         }
     }],
 
-    columns: [
-        {dataIndex: 'id', text: 'ID' },
-        {dataIndex: 'productCode', text: 'Product Code' , flex: 1},
-        {dataIndex: 'quantity', text: 'Quantity', flex: 2 , sortable: false,
-        renderer: function(value) {
-            if (parseInt(value) < 200) {
-              return '<span style="color: red; font-weight: 600;">' + value + '</span>';
-            } else {
-              return value;
-            }
-          }
+    plugins: {
+        cellediting: {
+            clicksToEdit: 2 //double click to edit data
+        }
     },
 
+    columns: [
+        { dataIndex: 'id', text: 'ID' },
+        { dataIndex: 'productCode', text: 'Product Code', flex: 1 },
+        { 
+            dataIndex: 'quantity', 
+            text: 'Quantity', 
+            flex: 2, 
+            sortable: false,
+            editor: {
+                xtype: 'numberfield',
+                allowBlank: false,
+                minValue: 0 
+            },
+            renderer: function(value) {
+                if (parseInt(value) < 200) {
+                    return '<span style="color: red; font-weight: 600;">' + value + '</span>';
+                } else {
+                    return value;
+                }
+            }
+        },
     ],
 
     selModel: {
@@ -38,7 +50,9 @@ Ext.define("Mini-shop.view.inventories.InventoriesGrid", {
         xtype: 'pagingtoolbar',
         displayInfo: true
     },
-    scrollable:true,
+    scrollable: true,
 
-
-})
+    listeners: {
+        edit: 'onEdit' 
+    }
+});
